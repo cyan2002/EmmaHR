1cdf3cf186324b5b55a0913655dd1d4fc49596d5
Chance Yan <chanceyan@vl965-172-31-66-49.wireless.umass.edu>
2025-03-05 16:40:48 -0500

    Adding Trial 7 and 8

--- 01 New Heartrate analysis script format agnostic.R
+++ 01 New Heartrate analysis script format agnostic.R
@@ -13,7 +13,7 @@ library(signal)
 
 # Change path below to the folder containing your converted raw data for a particular trial
 
-path <- "/Users/chanceyan/Documents/R/EmmaHR/EmmaHR/CardiacF(x)_converted/F2_Trial7_22C_converted"
+path <- "/Users/chanceyan/Documents/R/EmmaHR/EmmaHR/CardiacF(x)_converted/F2_Trial8_27C_converted"
 
 files <- list.files(path)
 dfs <- lapply(paste(path, files, sep="/"), read.table)
